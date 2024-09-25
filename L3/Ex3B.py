41d6b21ae4ace22fdae506c2fc2f3f94b60d557c
wts220828 <150510706+wts220828@users.noreply.github.com>
2024-09-25 19:20:51 +0800

    Create Ex3B.py

---
+++ L3/Ex3B.py
@@ -0,0 +1,11 @@
+#Q2
+print("Give me a number:")
+numbers=input("")
+print("Your number is "+numbers)
+
+#Q3
+print("Your birthday year?")
+birthday_year = input("")
+print("Your class nuumber?")
+class_number = input("")
+print("Your was born in "+birthday_year+" and your class number is "+class_number)
