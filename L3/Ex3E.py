5aec27b3d55cb3cb20aefb3c2a8623cf8497bc0b
wts220828 <150510706+wts220828@users.noreply.github.com>
2024-09-25 20:11:38 +0800

    Create Ex3E.py

---
+++ L3/Ex3E.py
@@ -0,0 +1,20 @@
+#Q1
+#this pound is not the biritish measure of weight, it is the currency of the british
+poundkg = 2.2
+print("How many pound of cake you want to buy?")
+pound = input("")
+print(pound+" pound = "+str(float(pound)/poundkg)+" kg")
+
+#Q2
+#from the SI prefix, 1m = 100cm
+cmm = 100
+print("How tall are you in cm?")
+cm = input("")
+print("You are "+str(float(cm)/cmm)+" m tall.")
+
+#Q3
+#from the SI prefix, 1m = 100cm
+cmm = 100
+print("How tall are you in cm?")
+cm = input("")
+print("You are "+str(float(cm)/cmm)+"m tall.")
