deeaf97cea607adc835fb5fc435e548dc6ae4ecb
wts220828 <150510706+wts220828@users.noreply.github.com>
2024-09-25 19:54:51 +0800

    Create EX3c.py

---
+++ L3/EX3c.py
@@ -0,0 +1,15 @@
+#Q1
+print("Give me a number:")
+num = input("")
+print(num+" x 10 = "+str(int(num)*10))
+
+#Q2
+print("Give me two numbers:")
+num1 = input("")
+num2 = input("")
+print(num1+" divided by "+num2+" = "+str(int(num1)//int(num2))+" ... "+str(int(num1)%int(num2)))
+
+#Q3
+print("Give me a number:")
+num = input("")
+print("The square number of "+num+" is "+str(int(num)**2))
