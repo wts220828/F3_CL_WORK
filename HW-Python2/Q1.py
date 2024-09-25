c6a67fdd78ce62bfa02c92f86166cd23bebd6425
wts220828 <150510706+wts220828@users.noreply.github.com>
2024-09-25 18:56:29 +0800

    Create Q1.py

---
+++ HW-Python2/Q1.py
@@ -0,0 +1,5 @@
+print("Which form are you in?")
+form = input("")
+print("How many students are in your class?")
+students = input("")
+print("You are a "+form+" student and you have "+students+" classmates.")
