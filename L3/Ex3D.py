47c90980469a5aa254009ee73574210e02185c5a
wts220828 <150510706+wts220828@users.noreply.github.com>
2024-09-25 20:03:02 +0800

    Create Ex3D.py

---
+++ L3/Ex3D.py
@@ -0,0 +1,17 @@
+#Q1
+USDHKD = 7.8
+print("How much USD do you have?")
+usd = input("")
+print(usd+" USD = "+str(int(usd)*USDHKD)+" HKD")
+
+#Q2
+USDHKD = 7.8
+print("How much HKD do you have?")
+hkd = input("")
+print(hkd+" HKD = "+str(int(hkd)/USDHKD)+" USD")
+
+#Q3
+JPYHKD = 200/11
+print("How much Japanese Yen do you have?")
+jpy = input("")
+print(jpy+" JPY = "+str(int(jpy)/JPYHKD)+" HKD")
